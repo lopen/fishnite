ea2816c99255fd07410398caf4af97937be13bd3
Joseph Dunning <jd551@kent.ac.uk>
2022-12-08 22:42:50 +0000

    jeoeoeoeoe

--- Assets/Scripts/World/GameHandler.cs
+++ Assets/Scripts/World/GameHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHandler : MonoBehaviour
 {
@@ -9,7 +10,17 @@ public class GameHandler : MonoBehaviour
     [SerializeField] public GameObject timer;
     [SerializeField] public GameObject player;
 
+    [SerializeField] private GameObject creditsLateCall;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject retryButton;
+    [SerializeField] private GameObject returnMenu;
+    [SerializeField] private GameObject gameOver;
+
+    [SerializeField] private GameObject healthGone;
+    [SerializeField] private GameObject timeGone;
+
     public bool gameRunning;
+    private int endStatus;
 
     // Start is called before the first frame update / 
     private void Start() {
@@ -32,23 +43,35 @@ public class GameHandler : MonoBehaviour
 
     IEnumerator GameListener() {
         while (gameRunning == true) {
-            print("second test");
-
             if(timer.GetComponent<Timer>().GetCurrentTime() == "0000") {
-                print("game over");
+                endStatus = 0;
                 EndGame();
             }
 
             if(player.GetComponent<PlayerHealth>().getHealth() == 0) {
-                print("health gone baby");
+                endStatus = 1;
                 EndGame();
             }
+
             yield return new WaitForSeconds(1f);
+            if (gameRunning == false && endStatus == 0) {
+                creditsLateCall.SetActive(true);
+                timeGone.SetActive(true);
+                gameOver.SetActive(true);
+                Time.timeScale = 0;
+            } else if (gameRunning == false && endStatus == 1) {
+                creditsLateCall.SetActive(true);
+                healthGone.SetActive(true);
+                gameOver.SetActive(true);
+                Time.timeScale = 0;
+            }
         }
     }
 
     public void EndGame() {
-
-
+        gameRunning = false;
+        creditsPanel.SetActive(true);
+        creditsPanel.GetComponent<Image>().canvasRenderer.SetAlpha(0f);
+        creditsPanel.GetComponent<Image>().CrossFadeAlpha(255.0f, 200.0f, false);
     }
 }
